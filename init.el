122095acafe9373978e2d9a7f0a7cbd0ec6c03c9
Simon Dahlberg <simon@hostit.se>
2010-03-04 15:38:34 +0100

    added files and stuff

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
 (add-to-list 'load-path "~/.emacs.d/plugins")
+(add-to-list 'load-path "~/.emacs.d")
 
 (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
           (let* ((my-lisp-dir "~/.emacs.d/plugins/")
@@ -12,3 +13,109 @@
 		           (color-theme-initialize)
 			        (color-theme-hober)))
 
+;; nxHtml
+(load "~/.emacs.d/plugins-nr/nxhtml/autostart.el")
+
+;; python
+;; (add-hook 'python-mode-hook guess-style-guess-tabs-mode)
+;;    (add-hook 'python-mode-hook (lambda ()
+;;                                   (when indent-tabs-mode
+;;                                     (guess-style-guess-tab-width)))
+
+;; Smart-tabs
+(require 'smart-tab)
+
+;; yasnippet
+(require 'yasnippet-bundle)
+
+(require 'python)
+
+;(require 'yasnippet)
+
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+
+;; Initialize Pymacs                                                                                           
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+;; Initialize Rope                                                                                             
+(pymacs-load "ropemacs" "rope-")
+(setq ropemacs-enable-autoimport t)
+
+;; Autocomplete
+;;     (require 'auto-complete)
+;;     (require 'auto-complete-config)
+;;     (global-auto-complete-mode t)
+(require 'auto-complete)
+(require 'auto-complete-config)
+(global-auto-complete-mode t)
+
+;; Line numbers
+(require 'linum)
+(global-set-key (kbd "<f6>") 'linum-mode)
+
+;; comment region
+(global-set-key (kbd "C-;") 'comment-dwim)
+
+;; run Django shell when editing Django Python code
+(defun get-file-in-upstream-dir (location filename)
+  (let* ((dir (file-name-directory location))
+         (path (concat dir filename)))
+    (if (file-exists-p path)
+        path
+      (if (not (equal dir "/"))
+        (get-file-in-upstream-dir (expand-file-name (concat dir "../")) filename)))))
+
+(defadvice run-python (before possibly-setup-django-project-environment)
+  (let* ((settings-py (get-file-in-upstream-dir buffer-file-name "settings.py"))
+         (project-dir (file-name-directory settings-py)))
+    (if settings-py
+        (progn
+          (setenv "DJANGO_SETTINGS_MODULE" "settings")
+          (setenv "PYTHONPATH" project-dir)))))
+
+
+;; Highlight paranteses
+(require 'highlight-parentheses)
+(defun turn-on-highlight-parentheses-mode ()
+  (highlight-parentheses-mode t))
+(define-global-minor-mode global-highlight-parentheses-mode
+  highlight-parentheses-mode
+  turn-on-highlight-parentheses-mode)
+(global-highlight-parentheses-mode)
+(setq hl-paren-background-colors '("green"))
+
+;; javascript mode
+(autoload #'espresso-mode "espresso" "Start espresso-mode" t)
+(add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
+
+;; my various functions
+(require 'various)
+;; breadcrumbs
+(require 'breadcrumb)
+
+
+;; Keyboard bindings
+(global-set-key (kbd "M-<right>") 'next-buffer)
+(global-set-key (kbd "M-<left>") 'previous-buffer)
+(global-set-key (kbd "C-M-<SPC>") 'speedbar)
+(global-set-key (kbd "<RET>") 'newline-and-indent)
+
+(put 'scroll-left 'disabled nil)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(majmodpri-sort-after-load (quote (chart gpl django-html-mumamo nxhtml-autoload javascript-mode))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
---
+++ various.el
@@ -0,0 +1,14 @@
+;; This buffer is for notes you don't want to save, and for Lisp evaluation.
+;; If you want to create a file, visit that file with C-x C-f,
+;; then enter the text in that file's own buffer.
+(require 'breadcrumb)
+(defun bookmark-and-jump-to-def ()
+  (interactive)
+  ; Add to bookmark to list
+  (bc-set)
+  (rope-goto-definition)
+  )
+(global-set-key [(meta j)] 'bookmark-and-jump-to-def)
+(global-set-key [(shift meta j)] 'bc-previous)
+
+(provide 'various)
