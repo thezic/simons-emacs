0665d0e158424f86cec5423e334c799db9e8f269
Simon Dahlberg <simon@plattan.localdomain>
2010-03-08 23:46:08 +0100

    inhibit start screen

--- init.el
+++ init.el
@@ -112,6 +112,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
  '(majmodpri-sort-after-load (quote (chart gpl django-html-mumamo nxhtml-autoload javascript-mode))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
