86c672ed2f69717a2e16363f46cd68290b5e5344
Simon Dahlberg <simon@hostit.se>
2010-05-27 10:58:57 +0200

    Set font

--- init.el
+++ init.el
@@ -126,6 +126,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(majmodpri-sort-after-load (quote (chart gpl django-html-mumamo nxhtml-autoload javascript-mode)))
  '(mouse-wheel-progressive-speed nil)
  '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
@@ -135,4 +136,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#c0c0c0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 92 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
